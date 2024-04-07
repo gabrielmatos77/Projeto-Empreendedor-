3a3aee2f49614290e815a66e81ef6969bd931808
Gabriel  Matos <145713183+gabrielmatos77@users.noreply.github.com>
2024-04-07 15:51:34 -0300

    Versao Com CSV app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-import json
+import csv
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -6,10 +6,20 @@ app = Flask(__name__)
 @app.route('/login', methods=['POST'])
 def login():
     dados = request.json
+    
     username = dados.get('username')
-    status = dados.get('status')
-    if username is None or status is None:
-        return jsonify({'mensagem': 'Dados incompletos'}), 400
-    return jsonify({'username': username, 'status': status}), 200
+    password = dados.get('password')
+    
+    if not username or not password:
+        return jsonify({'mensagem': 'Nome de usuário e senha são obrigatórios'}), 400
+
+    with open('usuarios.csv', mode='r', newline='') as arquivo_csv:
+        leitor_csv = csv.DictReader(arquivo_csv)
+        for linha in leitor_csv:
+            if linha['username'] == username and linha['password'] == password:
+                return jsonify({'mensagem': 'Login bem-sucedido', 'username': username, 'status': linha['status']}), 200
+
+    return jsonify({'mensagem': 'Credenciais inválidas'}), 401
+
 if __name__ == "__main__":
     app.run(port=5000)
