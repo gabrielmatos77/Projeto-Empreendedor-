6ae7a80ba134140878ec07a4b639fa24e9f5bfd7
Gabriel  Matos <145713183+gabrielmatos77@users.noreply.github.com>
2024-04-07 15:47:42 -0300

    Versao1 app.py

--- api/app.py
+++ api/app.py
@@ -3,11 +3,13 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
 @app.route('/login', methods=['POST'])
 def login():
-    return '', 200, {'token': ''}
-
-
+    dados = request.json
+    username = dados.get('username')
+    status = dados.get('status')
+    if username is None or status is None:
+        return jsonify({'mensagem': 'Dados incompletos'}), 400
+    return jsonify({'username': username, 'status': status}), 200
 if __name__ == "__main__":
     app.run(port=5000)
