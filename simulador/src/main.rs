61c9b3b6a761c7475eadf7eef5d1543879fbdc72
noriebalbinot <norie12@hotmail.com>
2024-04-09 21:19:25 -0300

    Simulador: adicionado contagem e troca de estado randomico

--- simulador/src/main.rs
+++ simulador/src/main.rs
@@ -1,34 +1,61 @@
+use core::f64;
+use rand::prelude::*;
 use std::{thread::sleep, time::Duration};
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Machine {
-    name: String,
+    name: &'static str,
     working: bool,
+    qtd: f64,
 }
 
 impl Machine {
-    fn new(name: String) -> Machine {
+    fn new(name: &'static str) -> Machine {
         Machine {
             name,
-            working: false,
+            working: rand::random(),
+            qtd: 0.0,
         }
     }
 
-    fn plot(mut self) {
-        self.working = !self.working
+    fn change(mut self) -> Self {
+        let rng: f64 = rand::thread_rng().gen();
+        let laststate = self.working;
+        if rng > 0.98 {
+            self.working = !self.working;
+        }
+        if self.working && laststate == self.working {
+            self.qtd = self.qtd + 1.0
+        } else {
+            self.qtd = 0.0
+        }
+
+        self
+    }
+    fn plot(self) -> Self {
+        println!(
+            "maq:{} funcionando:{} qtd: {}",
+            self.name, self.working, self.qtd
+        );
+        self
     }
 }
 
 fn main() {
+    let names = vec![
+        "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011",
+        "0012", "0013", "0014", "0015",
+    ];
+    let mut maqs: Vec<Machine> = vec![];
+    for i in 1..15 {
+        maqs.push(Machine::new(names[i]))
+    }
     loop {
-        let mut machines: Vec<Machine> = vec![];
-
-        for i in 1..5 {
-            machines.push(Machine::new(i.to_string()))
+        let mut intmaqs: Vec<Machine> = vec![];
+        for maq in maqs {
+            intmaqs.push(maq.change().plot())
         }
-        for maq in machines {
-            println!("maq:{} funcionando:{}", maq.name, maq.working)
-        }
+        maqs = intmaqs;
         sleep(Duration::new(2, 0))
     }
 }
