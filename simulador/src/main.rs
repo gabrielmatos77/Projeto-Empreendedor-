5d098f3693241402991b237625362920c3dd05e3
noriebalbinot <norie12@hotmail.com>
2024-04-17 22:29:37 -0300

    simulador: finalizado parte do envio para o banco de dados, TODO: arrumar o codigo que ta bagunçado '-'

--- simulador/src/main.rs
+++ simulador/src/main.rs
@@ -1,37 +1,40 @@
 use core::f64;
 use rand::prelude::*;
+use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, thread::sleep, time::Duration};
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 struct Machine {
-    name: &'static str,
+    name: String,
     working: bool,
     qtd: f64,
+    exec_id: String,
 }
 
 impl Machine {
-    fn new(name: &'static str) -> Machine {
+    fn new(name: String, working: bool, exec: String) -> Machine {
         Machine {
             name,
-            working: rand::random(),
+            working,
             qtd: 0.0,
+            exec_id: exec,
         }
     }
 
-    async fn change(self) -> Self {
+    async fn change(mut self) -> Self {
         let rng: f64 = rand::thread_rng().gen();
-        let changestate = rng > 0.48;
+        let changestate = rng > 0.99;
         match (self.working, changestate) {
             (true, true) => {
-                self.stop_machine().await;
+                self = self.stop_machine().await;
                 self
             }
             (false, true) => {
-                self.start_machine().await;
+                self = self.start_machine().await;
                 self
             }
             (true, false) => {
-                self.incremet_qtd();
+                self = self.incremet_qtd();
                 self
             }
             _ => self,
@@ -42,17 +45,19 @@ impl Machine {
         let nowdate = &chrono::Local::now().to_string();
         let thisqtd = &self.qtd.to_string();
         let mut data = HashMap::new();
-        data.insert("maquina", self.name);
+        data.insert("maquina", &self.name);
         data.insert("fim", nowdate);
         data.insert("qtd_prod", thisqtd);
         let client = reqwest::Client::new();
         let resp = client
-            .post("http://4.228.217.142:8090/api/collections/registro_producao/records")
+            .patch(format!(
+                "http://4.228.217.142:8090/api/collections/registro_producao/records/{}",
+                self.exec_id
+            ))
             .json(&data)
             .send()
             .await;
 
-        println!("{resp:#?}");
         self.working = false;
         self.qtd = 0.0;
         return self;
@@ -60,13 +65,19 @@ impl Machine {
     async fn start_machine(mut self) -> Self {
         //req to start
         let mut data = HashMap::new();
-        data.insert("maquina", self.name);
+        data.insert("maquina", &self.name);
         let client = reqwest::Client::new();
-        let _ = client
+        let record = client
             .post("http://4.228.217.142:8090/api/collections/registro_producao/records")
             .json(&data)
             .send()
-            .await;
+            .await
+            .expect("err")
+            .json::<ResponseStartMachine>()
+            .await
+            .ok()
+            .unwrap();
+        self.exec_id = record.id;
         self.working = true;
         self
     }
@@ -83,22 +94,92 @@ impl Machine {
     }
 }
 
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+struct PocketbaseRequest<T> {
+    page: u32,
+    perPage: u32,
+    totalItems: u32,
+    totalPages: u32,
+    items: Vec<T>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+struct MachineItens {
+    collectionId: String,
+    collectionName: String,
+    created: String,
+    endereco_ip_: String,
+    id: String,
+    maquina: String,
+    status_maquina: bool,
+    updated: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+struct NowMachineState {
+    collectionId: String,
+    collectionName: String,
+    created: String,
+    fim: String,
+    id: String,
+    maquina: Vec<String>,
+    qtd_prod: u32,
+    updated: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+struct ResponseStartMachine {
+    id: String,
+    collectionId: String,
+    collectionName: String,
+    created: String,
+    updated: String,
+    maquina: Vec<String>,
+    fim: String,
+    qtd_prod: u32,
+}
+
 #[tokio::main]
 async fn main() {
-    let names = vec![
-        "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011",
-        "0012", "0013", "0014", "0015",
-    ];
+    let dt = reqwest::get("http://4.228.217.142:8090/api/collections/maquinas/records")
+        .await
+        .expect("err")
+        .json::<PocketbaseRequest<MachineItens>>()
+        .await
+        .ok()
+        .unwrap();
     let mut maqs: Vec<Machine> = vec![];
-    for i in 1..15 {
-        maqs.push(Machine::new(names[i]))
+    for maq in dt.items {
+        let this_exec = reqwest::get(format!("http://4.228.217.142:8090/api/collections/registro_producao/records?filter=(fim=''%26%26maquina.id='{}')",maq.id))
+            .await
+            .expect("err")
+            .json::<PocketbaseRequest<NowMachineState>>()
+            .await;
+
+        let (id_exec, tot_itens) = match this_exec {
+            Ok(e) => {
+                if e.totalItems > 0 {
+                    (e.items.first().unwrap().id.to_string(), true)
+                } else {
+                    ("".to_string(), false)
+                }
+            }
+            Err(_) => ("".to_string(), false),
+        };
+
+        maqs.push(Machine::new(maq.id, tot_itens, id_exec));
     }
+
     loop {
         let mut intmaqs: Vec<Machine> = vec![];
         for maq in maqs {
             intmaqs.push(maq.change().await.plot())
         }
         maqs = intmaqs;
-        sleep(Duration::new(20, 0))
+        sleep(Duration::new(10, 0))
     }
 }
